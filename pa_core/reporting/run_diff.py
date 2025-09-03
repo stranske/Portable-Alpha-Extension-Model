01effbb1fffa05300cd201ec507d80b25a35728b
stranske <stranske@gmail.com>
2025-09-03 14:17:50 -0500

    fix: handle run diff delta calculation

--- pa_core/reporting/run_diff.py
+++ pa_core/reporting/run_diff.py
@@ -49,6 +49,8 @@ def build_run_diff(
             ):
                 cur_val = current_summary[col].iloc[0]
                 prev_val = previous_summary[col].iloc[0]
+                try:
+                    delta = cur_val - prev_val
                 except (TypeError, ValueError):
                     continue
                 metric_records.append(
