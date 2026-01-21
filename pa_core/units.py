dc0fccad13faef7455878cd8910bf444be629f45
stranske <stranske@gmail.com>
2026-01-21 06:36:09 +0000

    fix: align sleeve constraint metadata

--- pa_core/units.py
+++ pa_core/units.py
@@ -110,6 +110,10 @@ CONFIG_TIME_HORIZON_FIELDS: dict[str, str] = {
     "sleeve_max_breach": "monthly",
     "sleeve_max_cvar": "monthly",
     "financing_term_months": "months",
+    "sleeve_max_te": "monthly",
+    "sleeve_max_breach": "monthly",
+    "sleeve_max_cvar": "monthly",
+    "sleeve_max_shortfall": "annual",
 }
 
 
